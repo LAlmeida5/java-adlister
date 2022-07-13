f3f9b03038298822a9f1098d201f7543984f5443
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-13 13:53:45 -0500

    finished servlet exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,34 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    private int hitCount;
 
+    public void init(){
+        hitCount = 0;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        res.setContentType("text/html");
+
+        PrintWriter out = res.getWriter();
+        hitCount++;
+        String title = "Total number of Hits: " + hitCount;
+
+        out.println("<h1>" + title + "</h1>");
+
+        if(name != null) {
+            out.println("<h1>Hello, " + name + " </h1>");
+        }else {
+            out.println("<h1>Hello, World!</h1>");
+        }
     }
 }
