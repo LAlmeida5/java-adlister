c09b5adef19499487fa1c3b00a6546d32d2c4ea9
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-15 13:56:40 -0500

    update on pizza exercise

---
+++ src/main/java/PizzaOrderServlet.java
@@ -0,0 +1,13 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "PizzaOrderServlet", urlPatterns = "/pizza-order")
+public class PizzaOrderServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().println("<h1>Welcome to Pizza World!</h1>");
+    }
+}
